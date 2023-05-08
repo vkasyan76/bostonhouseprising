e88888df9779e2116f3afe15255513c68c9252ef
Valentyn Kasyan <vkasyan@gmx.de>
2023-05-08 21:36:14 +0200

    home.html local app

--- app.py
+++ app.py
@@ -34,5 +34,15 @@ def predict_api():
     return jsonify(output[0])
 
 
+@app.route('/predict', methods=['POST'])
+def predict():
+    data = [float(x) for x in request.form.values()]
+    final_input = scalar.transform(np.array(data).reshape(1, -1))
+    print(final_input)
+    output = regmodel.predict(final_input)[0]
+    print(output)
+    return render_template("home.html", prediction_text="The house price prediction is {}".format(output))
+
+
 if __name__ == "__main__":
     app.run(debug=True)
